621b493a0d9a8b4066cb256292d0d1f2a963083a
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-01-16 13:07:54 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -0,0 +1,19 @@
+def solution(my_string):
+    answer = 0
+    
+#     nList = my_string.split(" ")
+    
+#     n1 = int(nList[0])
+#     a = nList[1]
+#     n2 = int(nList[2])
+#     print(len(nList))
+#     print(n1, a, n2)
+    
+#     if a == '+':
+#         answer = n1 + n2
+#     else :
+#         answer = n1 - n2
+
+    answer = eval(my_string)
+    
+    return answer

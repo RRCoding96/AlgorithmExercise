031b7dc038f622c7f689fc4baf9d931bbf8d4840
Yoon-Uk.Kim <93039817+VicReznov@users.noreply.github.com>
2023-06-09 12:32:10 +0900

    [level 3] Title: 거스름돈, Time: 52.30 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    
+    int MOD = 1_000_000_007;
+    
+    public int solution(int n, int[] money) {
+        int answer = 0;
+        
+        int[][] dp = new int[money.length + 1][n + 1];
+        
+        for(int i = 1; i <= money.length; i++) {
+            for(int j = 0; j <= n; j++) {
+                if(j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    if(j < money[i - 1])
+                        dp[i][j] = dp[i - 1][j];
+                    else
+                        dp[i][j] = (dp[i - 1][j] + dp[i][j - money[i - 1]]) % MOD;
+                    }
+                }
+        }
+        
+        return dp[money.length][n];
+    }
+}

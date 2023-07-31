beaa9c4d34e1caa31d219e385e90421fe2ee1b2b
Yoon-Uk.Kim <93039817+RRCoding96@users.noreply.github.com>
2023-07-31 20:57:24 +0900

    Time: 832 ms (83.40%), Space: 0B (100.00%) - LeetHub

---
+++ 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select round( (a.immediate / b.total) * 100.0, 2) as immediate_percentage 
+from (
+    select count(*) as immediate 
+     from delivery 
+     where order_date = customer_pref_delivery_date
+    ) as a,
+(select count(*) as total 
+ from delivery) as b

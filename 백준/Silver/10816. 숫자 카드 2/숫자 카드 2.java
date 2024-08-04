3796033a27aa5e44046517c6c8a82d278444499c
Yoon-Uk.Kim <reznov03048@gmail.com>
2024-08-04 15:13:51 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1356 ms, Memory: 123716 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,5 +1,5 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -8,62 +8,59 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine()); // 가지고 있는 숫자 카드 개수
-
+        int N = Integer.parseInt(br.readLine());
         int[] cards = new int[N];
         st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0; i < N; i++) {
             cards[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(cards);
 
-        int M = Integer.parseInt(br.readLine()); // 구해야 되는 수
-
-        int[] targets = new int[N];
+        int M = Integer.parseInt(br.readLine());
+        int[] hands = new int[M];
         st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0; i < M; i++) {
-            targets[i] = Integer.parseInt(st.nextToken());
-            int result = findByBinarySearch(cards, 0, N - 1, targets[i]);
+            hands[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(cards);
+
+        for(int i = 0; i < hands.length; i++) {
+            int result = findCount(cards, hands[i]);
             sb.append(result).append(" ");
         }
 
-        System.out.println(sb);
-
+        System.out.println(sb.toString());
     }
 
-    static int findByBinarySearch(int[] arr, int start, int end, int target) {
+    private static int findCount(int[] cards, int target) {
+        int s = 0;
+        int e = cards.length;
 
-        int s = start;
-        int e = end + 1;
-
-        // 최저 인덱스 찾기
-        while(s < e) {
+        // 최소
+        while (s < e) {
             int mid = (s + e) / 2;
-
-            if(target <= arr[mid]) {
+            if(target <= cards[mid]) {
                 e = mid;
             } else {
                 s = mid + 1;
             }
         }
-
-        int minIdx = s;
-
-        // 최고 인덱스 찾기
-        s = start;
-        e = end + 1;
-
-        while(s < e) {
+        int minIndex = s;
+        // 최대
+        s = 0;
+        e = cards.length;
+        while (s < e) {
             int mid = (s + e) / 2;
-            if(arr[mid] > target) {
+            if(target < cards[mid]) {
                 e = mid;
             } else {
                 s = mid + 1;
             }
         }
+        int maxIndex = s;
 
-        int maxIdx = s;
 
-        return maxIdx - minIdx;
+        return maxIndex - minIndex;
     }
+
 }

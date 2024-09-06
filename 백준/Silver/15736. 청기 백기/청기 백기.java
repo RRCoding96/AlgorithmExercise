09506798f37f01b407eae17c6f04679d2d508014
Yoon-Uk.Kim <reznov03048@gmail.com>
2024-09-06 17:59:35 +0900

    [Silver IV] Title: 청기 백기, Time: 96 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int a = (int) Math.sqrt(N);
+
+        System.out.println(a);
+    }
+}

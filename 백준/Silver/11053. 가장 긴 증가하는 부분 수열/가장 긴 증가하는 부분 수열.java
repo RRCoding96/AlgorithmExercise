5723129e52d117e2a5dfff57e821bc25aaf1e62f
Yoon-Uk.Kim <reznov03048@gmail.com>
2024-09-28 11:58:56 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,44 +1,42 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int N;
-	static int[] arr;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		
-		arr = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int[] dp = new int[1000];
-		for (int i=0; i<1000; i++) {
-			dp[i] = 1;
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		int answer = 1;
-		for (int i=1; i<N; i++) {
-			for (int j = 0; j < i; j++) {
-				if (arr[i] > arr[j]) {
-					dp[i] = Math.max(dp[j] + 1, dp[i]);
-				}
-				if (dp[i] > answer) {
-					answer = dp[i];
-				}
-			}
-		}
-		
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 길이 저장할 배열
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+
+        int answer = 1;
+        // 오른쪽(기준)
+        for (int i = 1; i < n; i++) {
+            // 왼쪽(탐색할 인덱스)
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+                if (dp[i] > answer) {
+                    answer = dp[i];
+                }
+            }
+        }
+
 //		System.out.println(Arrays.toString(dp));
-		System.out.println(answer);
-	}
-	
-	
+        System.out.println(answer);
+    }
 
 }

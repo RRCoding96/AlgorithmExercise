130422787b8f43a7957f8ec70c8d5a9a734c2019
Yoon-Uk.Kim <reznov03048@gmail.com>
2024-10-26 20:53:32 +0900

    [Gold V] Title: LCS, Time: 128 ms, Memory: 18308 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,32 +1,39 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		char[] A = br.readLine().toCharArray();
-		char[] B = br.readLine().toCharArray();
-		
-		int[][] dp = new int[A.length + 1][B.length + 1];
-		
-		int answer = 0;
-		for(int a=0; a<A.length; a++) {
-			for(int b=0; b<B.length; b++) {
-				if(A[a] == B[b]) {
-					dp[a + 1][b + 1] = dp[a][b] + 1;
-				} else {
-					dp[a + 1][b + 1] = Math.max(dp[a][b + 1], dp[a+1][b]);
-				}
-				
-				answer = Math.max(answer, dp[a + 1][b + 1]);
-			}
-		}
-		
-		System.out.println(answer);
-		
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        char[] str1 = br.readLine().toCharArray();
+        char[] str2 = br.readLine().toCharArray();
+
+        int length_1 = str1.length;
+        int length_2 = str2.length;
+
+        // 공집합 표현을 위해 인덱스가 한 줄씩 추가 됨
+        int[][] dp = new int[length_1 + 1][length_2 + 1];
+
+        // 1부터 시작 (index 0 은 공집합이므로 0의 값을 갖고있음)
+        for(int i = 1; i <= length_1; i++) {
+            for(int j = 1; j <= length_2; j++) {
+
+                // (i-1)과 (j-1) 번째 문자가 서로 같다면
+                if(str1[i - 1] == str2[j - 1]) {
+                    // 대각선 위 (i-1, j-1)의 dp에 +1 한 값으로 갱신
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+
+                // 같지 않다면 이전 열(i-1)과 이전 행(j-1)의 값 중 큰 것으로 갱신
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[length_1][length_2]);
+    }
+
 }

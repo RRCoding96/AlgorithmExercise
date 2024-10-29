889d06f7be055ebd63f6ebbe35bf7df17f5b1225
Yoon-Uk.Kim <reznov03048@gmail.com>
2024-10-29 16:48:02 +0900

    [Silver V] Title: 숫자 카드, Time: 1208 ms, Memory: 106228 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -3,61 +3,49 @@ import java.util.*;
 
 public class Main {
 
-    static int N;
-    static int[] cards;
-    static int M;
-    static int[] hands;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
-        cards = new int[N];
+        int N = Integer.parseInt(br.readLine());
+        int[] hands = new int[N];
         st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0; i < N; i++) {
-            cards[i] = Integer.parseInt(st.nextToken());
-        }
-
-        M = Integer.parseInt(br.readLine());
-        hands = new int[M];
-        st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < M; i++) {
             hands[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(hands);
 
         StringBuilder sb = new StringBuilder();
-        Arrays.sort(cards);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0; i < M; i++) {
-            int now = hands[i];
-            if(binarySearch(now)) {
-                sb.append(1).append(" ");
-            } else {
-                sb.append(0).append(" ");
-            }
+            int target = Integer.parseInt(st.nextToken());
+
+            int result = binarySearch(N, hands, target);
+            sb.append(result).append(" ");
         }
 
         System.out.println(sb);
-
     }
 
-    static boolean binarySearch(int target) {
-        int s = 0;
-        int e = N - 1;
-        
-        while(s <= e) {
-            int mid = (s + e) / 2;
-            int num = cards[mid];
-            if(num == target) {
-                return true;
-            } else if(num < target) {
-                s = mid + 1;
+    private static int binarySearch(int N, int[] arr, int target) {
+        int start = 0;
+        int end = N - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int value = arr[mid];
+            if(value == target) {
+                return 1;
+            } else if(value > target) {
+                end = mid - 1;
             } else {
-                e = mid - 1;
+                start = mid + 1;
             }
         }
-        
-        return false;
+
+        return 0;
     }
 
 }

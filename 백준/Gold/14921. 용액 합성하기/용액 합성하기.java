faff296f593f84c103692319ed10ae9dbaf45813
Yoon-Uk.Kim <reznov03048@gmail.com>
2024-12-18 21:41:17 +0900

    [Gold V] Title: 용액 합성하기, Time: 316 ms, Memory: 26132 KB -BaekjoonHub

---
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        long[] arr = new long[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        long minDiff = Long.MAX_VALUE;
+        long answer = 0;
+
+        int start = 0;
+        int end = N - 1;
+        while (start < end) {
+            long midValue = arr[start] + arr[end];
+
+            if(minDiff > Math.abs(midValue)) {
+                answer = midValue;
+                minDiff = Math.abs(midValue);
+            }
+
+            if(midValue > 0) {
+                end--;
+            } else {
+                start++;
+            }
+
+        }
+
+        System.out.println(answer);
+    }
+
+}

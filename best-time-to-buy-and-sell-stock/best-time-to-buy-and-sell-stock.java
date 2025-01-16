dd1eb97fc0bf3e52e09d50be675bd4ad5678f56a
Yoon-Uk.Kim <reznov03048@gmail.com>
2025-01-16 16:43:52 +0900

    Time: 1 ms, Memory: 62 MB - LeetHub

---
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        
+        int answer = 0;
+        for(int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            answer = Math.max(answer, price - minPrice);
+        }
+        
+        return answer;
+    }
+}

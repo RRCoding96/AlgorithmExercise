28f04a7386bfa6c513d4cd336c7a2b89cfd99f63
Yoon-Uk.Kim <reznov03048@gmail.com>
2025-02-06 11:33:36 +0900

    Time: 2 ms, Memory: 44.5 MB - LeetHub

---
+++ permutations/permutations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        backtracking(nums, result, new ArrayList<>());
+        
+        return result;
+    }
+
+    public void backtracking(int[] nums, List<List<Integer>> result, List<Integer> list) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int num : nums) {
+            if (list.contains(num)) continue;
+
+            list.add(num);
+            
+            backtracking(nums, result, list);
+            
+            list.remove(list.size() - 1);
+        }
+    }
+}

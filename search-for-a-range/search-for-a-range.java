865c9a113657251ac6087a9d3124735133a47cd0
Yoon-Uk.Kim <reznov03048@gmail.com>
2025-02-11 14:57:00 +0900

    Time: 0 ms, Memory: 45.9 MB - LeetHub

--- search-for-a-range/search-for-a-range.java
+++ search-for-a-range/search-for-a-range.java
@@ -1,37 +1,53 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
-            return new int[] { -1, -1 };
+            return new int[]{-1, -1};
         }
-        
-        int left = searchInsert(nums, target);
-        if (left < nums.length && nums[left] == target) { 
-            return new int[] { left, searchInsert(nums, target + 1) - 1 }; 
-        } else {
-            return new int[] { -1, -1 };
+
+        int start = lowerBinarySearch(nums, target);
+        int end = upperBinarySearch(nums, target) - 1;
+
+        // target이 배열 내에 없는 경우 체크
+        if (start >= nums.length || nums[start] != target) {
+            return new int[]{-1, -1};
         }
+
+        return new int[]{start, end};
     }
     
-    private int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left + 1 < right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] < target) {
-                left = mid;
+    // target의 첫 번째 위치
+    private int lowerBinarySearch(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (nums[mid] >= target) {
+                e = mid - 1;
             } else {
-                right = mid;
+                s = mid + 1;
             }
         }
-        
-        if (nums[left] >= target) {
-            return left;
-        } else if (nums[right] >= target) {
-            return right;
-        } else {
-            return right + 1;
+
+        return s;
+    }
+
+    // target 보다 큰 값이 처음 등장하는 위치
+    private int upperBinarySearch(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (nums[mid] <= target) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
         }
+
+        return s;
     }
 }
